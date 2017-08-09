e724beb0c548abb67febd3a768f67aa4a27f8b7d
ISABELLE <isabelle.k.miller@gmail.com>
2017-08-09 12:53:47 -0700

    added some new react outline files

---
+++ static/js/components/Card.js
@@ -0,0 +1,28 @@
+require('../styles/card.css')
+
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Card extends Component {
+  static propTypes = {
+
+  }
+}
+
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      // insert JSX here
+      <div className='card-container'>
+      I SHALL HOLD SOME INFOS AT SOME FUTURE INDETERMINATE POINT
+      </div>
+    )
+  }
+
+  export default Card
---
+++ static/js/components/TopNav.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class TopNav extends Component {
+  static propTypes = {
+
+  }
+}
+
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      // insert JSX here
+    )
+  }
+
+  export default TopNav
