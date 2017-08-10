d1ab4d7172f700f114837a989dde9b779d4d6ab4
Leah Yukelson <leahyukelson@gmail.com>
2017-08-10 12:27:43 -0700

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,38 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
-var config = {
-  entry: APP_DIR + '/static/app.js',
+module.exports = {
+  entry: [
+    './static/app.js'
+  ],
   output: {
-    path: BUILD_DIR,
+    path: __dirname + '/static/build',
+    publicPath: '/',
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'es2016', 'stage-0']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'styles.css',
+      disable: false,
+      allChunks: true
+    }), 
+  ]
 };
-
-module.exports = config;
