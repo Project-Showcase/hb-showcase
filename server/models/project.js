b6617b8cc02a70abd87c977ec5ab34a713f3ac5f
Leah Yukelson <leahyukelson@gmail.com>
2017-08-15 15:35:24 -0700

    Seeding initial projects

--- server/models/project.js
+++ server/models/project.js
@@ -25,5 +25,17 @@ module.exports = (sequelize, DataTypes) => {
       }
     }
   });
+  Project.bulkCreate([{ 
+        projectGithub: 'https://github.com/sloloris/hb-final-project',
+        title: 'FriendKeeper',
+        description: 'The email-automating web application for those of us who struggle to keep in touch with the people who matter!',
+        userId: 1
+      },
+      {
+        projectGithub: 'https://github.com/leahyukelson/Night-Out',
+        title: 'Night Out',
+        description: 'A web application that allows a user to plan a night around a pre-planned event by combining the event with nearby restaurant or bar options using the Yelp API and sends an email itinerary to friends with SendGrid API.',
+        userId: 2
+      }])
   return Project;
 };
