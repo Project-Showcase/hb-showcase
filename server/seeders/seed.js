239a410e5aa0f2ab7b763c6edf599c434ef9fd6c
ISABELLE <isabelle.k.miller@gmail.com>
2017-08-15 15:34:40 -0700

    experimenting with separating out the seed data

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,17 +1,18 @@
+
+const Project = require('../models/project')
+
 module.exports = (queryInterface, Sequelize) => {
-  var populateDB = queryInterface.bulkInsert('User', [
-      { 
-        firstName: 'Isabelle',
-        lastName: 'Miller',
-        github: 'sloloris',
-        linkedIn: 'https://www.linkedin.com/in/isabelle-miller/'
+  var populateDB = Project.bulkCreate([{ 
+        projectGithub: 'https://github.com/sloloris/hb-final-project',
+        title: 'FriendKeeper',
+        description: 'The email-automating web application for those of us who struggle to keep in touch with the people who matter!',
+        userId: 1
       },
       {
-        firstName: 'Leah',
-        lastName: 'Yukelson',
-        github: 'leahyukelson',
-        linkedIn: 'https://www.linkedin.com/in/leahyukelson/'
-      }
-      ]);
+        projectGithub: 'https://github.com/leahyukelson/Night-Out',
+        title: 'Night Out',
+        description: 'A web application that allows a user to plan a night around a pre-planned event by combining the event with nearby restaurant or bar options using the Yelp API and sends an email itinerary to friends with SendGrid API.',
+        userId: 2
+      }]);
   return populateDB
 };
