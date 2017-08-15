2d8e6ced4445e66751e851543bf423a33d95203d
Leah Yukelson <leahyukelson@gmail.com>
2017-08-15 16:15:59 -0700

    Adding new testing route with base html code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 // Set up the express app
 const app = express();
@@ -13,8 +14,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
+app.get('/', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
+
+app.get('/testing', function(req, res) {
+  res.sendFile(path.join(__dirname + '/static/templates/base.html'))
+});
+
 
 module.exports = app;
